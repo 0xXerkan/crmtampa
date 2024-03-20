36aca80d9788fd1f180142838d543a1eda074643
Ricky Landry <104511702+0xXerkan@users.noreply.github.com>
2024-03-19 21:38:31 -0400

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
+import RequestAQuote from './RequestAQuote';
 
 const Navbar = () => {
 
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setModalOpen(!modalOpen);
+  }
+
   return (
-    <nav className="navbar navbar-expand-lg bg-black navbar-dark sticky-top py-2 border-bottom border-primary border-3">
-      <div className="container">
-        <Link to="/" className='navbar-brand m-0 p-0'>
-          <div className='d-flex align-items-center navbar-brand m-0'>
-            <div className="navbar-brand border-end border-light border-2 pe-2 m-0 __nav-small">CRM</div>
-            <div className='navbar-brand m-0 mx-1 __nav-small'>
-              <img src="/logo.png" alt="logo" height='46' width='auto' />
+    <div>
+      <nav className="navbar navbar-expand-lg bg-black navbar-dark sticky-top py-2 border-bottom border-primary border-3">
+        <div className="container">
+          <Link to="/" className='navbar-brand m-0 p-0'>
+            <div className='d-flex align-items-center navbar-brand m-0'>
+              <div className="navbar-brand border-end border-light border-2 pe-2 m-0 __nav-small">CRM</div>
+              <div className='navbar-brand m-0 mx-1 __nav-small'>
+                <img src="/logo.png" alt="logo" height='46' width='auto' />
+              </div>
+              <div className="navbar-brand m-0 __nav-small">Cerberus Risk Management</div>
             </div>
-            <div className="navbar-brand m-0 __nav-small">Cerberus Risk Management</div>
+          </Link>
+
+
+          <button className="navbar-toggler px-0" type="button" data-bs-toggle="collapse" data-bs-target="#navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navigation">
+            <ul className="navbar-nav ms-auto d-lg-flex align-items-lg-center">
+              <li className="nav-item">
+                <a href="/#services" className="nav-link">Services</a>
+              </li>
+              <li className="nav-item">
+                <div className="nav-link" onClick={toggleModal}>Request a Quote</div>
+              </li>
+              <li className="nav-item">
+                <a href="/#contact" className="nav-link">Contact Us</a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link">Careers</a>
+              </li>
+              <li className="nav-item">
+                <a href="tel:(401) 588-2449" className="nav-link">
+                  <button className='btn btn-primary' type='button'>(401) 588-2449</button>
+                </a>
+              </li>
+            </ul>
           </div>
-        </Link>
-
-
-        <button className="navbar-toggler px-0" type="button" data-bs-toggle="collapse" data-bs-target="#navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navigation">
-          <ul className="navbar-nav ms-auto d-lg-flex align-items-lg-center">
-            <li className="nav-item">
-              <a href="/#services" className="nav-link">Services</a>
-            </li>
-            <li className="nav-item">
-              <Link to='/request-a-quote' className="nav-link">Request a Quote</Link>
-            </li>
-            <li className="nav-item">
-              <a href="/#contact" className="nav-link">Contact Us</a>
-            </li>
-            <li className="nav-item">
-              <a href="/" className="nav-link">Careers</a>
-            </li>
-            <li className="nav-item">
-              <a href="tel:(401) 588-2449" className="nav-link">
-                <button className='btn btn-primary' type='button'>(401) 588-2449</button>
-              </a>
-            </li>
-          </ul>
         </div>
-      </div>
-    </nav>
+      </nav>
+      { modalOpen && <RequestAQuote toggleModal={toggleModal} />}
+    </div>
   )
 }
 
