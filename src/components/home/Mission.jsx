486468a90566e2b29831921495b59f509d2c4b84
Ricky Landry <104511702+0xXerkan@users.noreply.github.com>
2024-04-09 22:01:37 -0400

    Updates

--- src/components/home/Mission.jsx
+++ src/components/home/Mission.jsx
@@ -8,19 +8,19 @@ const Mission = () => {
         <div className="text-center">
           <h1 className='text-primary'>Our Mission</h1>
   
-          <p className="lead">
-          Welcome to Cerberus Risk Management, founded in Tampa, Florida. As one of the premiere executive protection and security companies, our mission is to safeguard your personnel and property with unparalleled attention to detail, discipline, quick thinking, and decision making, honed through multiple successful tours of duty to foreign wars as members of the most premiere fighting teams on the planet.
+          <p>
+          Welcome to Cerberus Risk Management, founded in Tampa, Florida. As one of the premier executive protection and security companies, our mission is to safeguard your personnel and property with unparalleled attention to detail, discipline, quick thinking, and decision-making, honed through multiple successful tours of duty in foreign wars as members of premier fighting teams.
           </p>
   
-          <p className="lead">
+          <p>
           We are proud to employ licensed, highly trained, and professional officers who are experts in loss prevention, risk mitigation, conflict resolution, and much more. Our team's backgrounds are second to none, allowing us to execute complex planning, logistics tracking, and security with ease and precision.
           </p>
   
-          <p className="lead">
-          At Cerberus Risk Management, we take a proactive approach to customer service, conducting comprehensive security assessments to tailor our services to your specific needs. Our extensive network of security officers across the country ensure that we are always ready and qualified to respond to any situation, 24/7.
+          <p>
+          At Cerberus Risk Management, we take a proactive approach to customer service, conducting comprehensive security assessments to tailor our services to your specific needs. Our extensive network of security officers across the country ensures that we are always ready and qualified to respond to any situation, 24/7.
           </p>
   
-          <p className="lead">
+          <p>
           When you choose Cerberus Risk Management, you gain a true partner in security. Our business development team is ready to discuss your unique security needs and provide a free site analysis to ensure that we provide the best possible value for protection services. Contact us today to experience the Cerberus difference.
           </p>
         </div>
